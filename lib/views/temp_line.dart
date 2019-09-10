454b737c563841de122049db633a60f3a32ad386
670453367 <670453367@qq.com>
2019-09-10 20:38:30 +0800

    自定义天气折线图widget

--- lib/views/temp_line.dart
+++ lib/views/temp_line.dart
@@ -35,105 +35,101 @@ class TempLinePainter extends CustomPainter {
     // 画线
     Paint linePaint = new Paint()
       ..style = PaintingStyle.stroke
-      ..color = Colors.red
+      ..color = Colors.white60
       ..strokeWidth = 1.0;
 
     // 画点
     Paint dotPaint = new Paint()
       ..style = PaintingStyle.fill
-      ..color = Colors.red;
+      ..color = Colors.white70;
 
-    // 画背景
+    // 画背景颜色
     Paint bgPaint = new Paint()
       ..style = PaintingStyle.fill
-      ..color = Colors.red
-      ..strokeWidth = 1.0;
-
-
+      ..color = Colors.white10;
     Path path = new Path();
-    path.moveTo(0, dots.elementAt(0));
 
     for (int i = 0; i < dots.length - 1; i++) {
+      double x = width * i + width / 2;
 
+      if (i == 0) {
+        canvas.drawLine(Offset(0, dots.elementAt(i)),
+            Offset(x, dots.elementAt(i)), linePaint);
 
-      for (int i = 0; i < dots.length - 1; i++) {
-        double x = width * i + width / 2;
+        canvas.drawLine(Offset(0, dots2.elementAt(i) + 50),
+            Offset(x, dots2.elementAt(i) + 50), linePaint);
+      } else if (i == dots.length - 2) {
+        canvas.drawLine(Offset(x + width, dots.elementAt(i + 1)),
+            Offset(x + width + width / 2, dots.elementAt(i + 1)), linePaint);
 
-        if (i == 0) {
-          canvas.drawLine(Offset(0, dots.elementAt(i)),
-              Offset(x, dots.elementAt(i)), linePaint);
-
-          canvas.drawLine(Offset(0, dots2.elementAt(i) + 50),
-              Offset(x, dots2.elementAt(i) + 50), linePaint);
-        } else if (i == dots.length - 2) {
-          canvas.drawLine(Offset(x + width, dots.elementAt(i + 1)),
-              Offset(x + width + width / 2, dots.elementAt(i + 1)), linePaint);
-
-          canvas.drawLine(
-              Offset(x + width, dots2.elementAt(i + 1) + 50),
-              Offset(x + width + width / 2, dots2.elementAt(i + 1) + 50),
-              linePaint);
-        }
-
-        canvas.drawLine(Offset(x, dots.elementAt(i)),
-            Offset(x + width, dots.elementAt(i + 1)), linePaint);
-
-        canvas.drawLine(Offset(x, dots2.elementAt(i) + 50),
-            Offset(x + width, dots2.elementAt(i + 1) + 50), linePaint);
+        canvas.drawLine(
+            Offset(x + width, dots2.elementAt(i + 1) + 50),
+            Offset(x + width + width / 2, dots2.elementAt(i + 1) + 50),
+            linePaint);
       }
 
-      for (int i = 0; i < dots.length; i++) {
-        double x = width * i + width / 2;
+      canvas.drawLine(Offset(x, dots.elementAt(i)),
+          Offset(x + width, dots.elementAt(i + 1)), linePaint);
 
-        canvas.drawCircle(
-          Offset(x, dots.elementAt(i)),
-          2,
-          dotPaint,
-        );
-
-        path.lineTo(x, dots.elementAt(i));
-
-        if (i == dots.length - 1) {
-          path.lineTo(x + width / 2, dots.elementAt(i));
-          path.lineTo(x + width / 2, dots.elementAt(i) + 50);
-        }
-
-        canvas.drawCircle(
-          Offset(x, dots2.elementAt(i) + 50),
-          2,
-          dotPaint,
-        );
-
-        // 新建一个段落建造器，然后将文字基本信息填入;
-        ParagraphBuilder pb = ParagraphBuilder(ParagraphStyle(
-          textAlign: TextAlign.left,
-          fontWeight: FontWeight.w300,
-          fontStyle: FontStyle.normal,
-          fontSize: 12,
-        ));
-        pb.pushStyle(ui.TextStyle(color: Colors.red));
-
-        pb.addText('${dots.elementAt(i)}');
-        // 设置文本的宽度约束
-        ParagraphConstraints pc = ParagraphConstraints(width: 30);
-        // 这里需要先layout,将宽度约束填入，否则无法绘制
-        Paragraph paragraph = pb.build()..layout(pc);
-        // 文字左上角起始点
-        Offset offset = Offset(x - paragraph.width / 2, dots.elementAt(i) - 20);
-        canvas.drawParagraph(paragraph, offset);
-
-        Offset offset2 = Offset(x - paragraph.width / 2, dots2.elementAt(i) + 60);
-        canvas.drawParagraph(paragraph, offset2);
-      }
-
-      for (int i = 0; i < dots.length; i++) {
-        double x = width * i + width / 2;
-
-        path.lineTo(x, dots2.elementAt(i));
-      }
-      path.close();
-      canvas.drawPath(path, bgPaint);
+      canvas.drawLine(Offset(x, dots2.elementAt(i) + 50),
+          Offset(x + width, dots2.elementAt(i + 1) + 50), linePaint);
     }
+
+    for (int i = 0; i < dots.length; i++) {
+      double x = width * i + width / 2;
+
+      // 画点
+      canvas.drawCircle(
+        Offset(x, dots.elementAt(i)),
+        2,
+        dotPaint,
+      );
+
+      canvas.drawCircle(
+        Offset(x, dots2.elementAt(i) + 50),
+        2,
+        dotPaint,
+      );
+
+      // 画文字
+      ParagraphBuilder pb = ParagraphBuilder(ParagraphStyle(
+        textAlign: TextAlign.left,
+        fontWeight: FontWeight.w300,
+        fontStyle: FontStyle.normal,
+        fontSize: 12,
+      ));
+      pb.pushStyle(ui.TextStyle(color: Colors.white70));
+
+      pb.addText('${dots.elementAt(i) - 70}');
+      ParagraphConstraints pc = ParagraphConstraints(width: 30);
+      Paragraph paragraph = pb.build()..layout(pc);
+      Offset offset = Offset(x - paragraph.width / 2, dots.elementAt(i) - 20);
+      canvas.drawParagraph(paragraph, offset);
+
+      Offset offset2 = Offset(x - paragraph.width / 2, dots2.elementAt(i) + 60);
+      canvas.drawParagraph(paragraph, offset2);
+    }
+
+    path.moveTo(0, dots.elementAt(0));
+
+    for (int i = 0; i < dots.length; i++) {
+      double x = width * i + width / 2;
+
+      path.lineTo(x, dots.elementAt(i));
+    }
+
+    path.lineTo(size.width, dots.elementAt(5));
+    path.lineTo(size.width, dots2.elementAt(5) + 50);
+
+    for (int i = 0; i < dots2.length; i++) {
+      double x = width * (5 - i) + width / 2;
+
+      path.lineTo(x, dots2.elementAt(5 - i) + 50);
+    }
+
+    path.lineTo(0, dots2.elementAt(0) + 50);
+    path.close();
+    canvas.drawPath(path, bgPaint);
   }
 
   @override
