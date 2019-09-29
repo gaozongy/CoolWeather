13772c0a875f50f38b845a87a80fa3d27c7eeb70
gl <670453367@qq.com>
2019-09-29 16:12:40 +0800

    修改已关注城市列表UI

--- lib/focus_district_list.dart
+++ lib/focus_district_list.dart
@@ -47,7 +47,9 @@ class _FocusDistrictListState extends State<FocusDistrictList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
+          elevation: 1,
           title: Text('选择城市'),
         ),
         body: focusCountyListBean != null
@@ -58,27 +60,45 @@ class _FocusDistrictListState extends State<FocusDistrictList> {
                       focusCountyListBean.districtList.elementAt(position);
                   return getRow(county);
                 })
-            : Text('empty'),
+            : Center(),
         floatingActionButton: FloatingActionButton(
             onPressed: _selectDistrict, child: new Icon(Icons.add)));
   }
 
   Widget getRow(District county) {
-    return new InkWell(
-      child: new Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-        child: new Text(county.name, style: new TextStyle(fontSize: 16)),
-      ),
-      onTap: () {
-        setState(() {
-          focusCountyListBean.districtList.remove(county);
-          Future<SharedPreferences> future = SharedPreferences.getInstance();
-          future.then((prefs) {
-            prefs.setString(
-                'focus_district_data', jsonEncode(focusCountyListBean));
-          });
-        });
-      },
+    return Card(
+      margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+      clipBehavior: Clip.antiAlias,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8))),
+      child: Container(
+          child: InkWell(
+              child: SizedBox(
+                height: 120,
+                width: double.infinity,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  child: Text(county.name,
+                      style: new TextStyle(color: Colors.white, fontSize: 16)),
+                ),
+              ),
+              onTap: () {
+                setState(() {
+//              focusCountyListBean.districtList.remove(county);
+//              Future<SharedPreferences> future = SharedPreferences
+//                  .getInstance();
+//              future.then((prefs) {
+//                prefs.setString(
+//                    'focus_district_data', jsonEncode(focusCountyListBean));
+//              });
+                });
+              }),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('image/sunny.jpg'),
+            fit: BoxFit.fitWidth,
+          ))),
     );
   }
 }
