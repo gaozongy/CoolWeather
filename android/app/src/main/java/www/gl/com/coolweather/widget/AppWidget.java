5533fc1546568b324a7070c322f99cb49ccff846
gl <670453367@qq.com>
2019-10-23 10:32:35 +0800

    1.小部件自带定位 2.修复小部件没有定位时无任何点击事件的问题

--- android/app/src/main/java/www/gl/com/coolweather/widget/AppWidget.java
+++ android/app/src/main/java/www/gl/com/coolweather/widget/AppWidget.java
@@ -1,17 +1,21 @@
 package www.gl.com.coolweather.widget;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
-import com.google.gson.Gson;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -24,7 +28,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import www.gl.com.coolweather.MainActivity;
 import www.gl.com.coolweather.R;
 import www.gl.com.coolweather.bean.Daily;
-import www.gl.com.coolweather.bean.District;
 import www.gl.com.coolweather.bean.Realtime;
 import www.gl.com.coolweather.bean.Skycon;
 import www.gl.com.coolweather.bean.Temperature;
@@ -53,43 +56,65 @@ public class AppWidget extends AppWidgetProvider {
 
         String action = intent.getAction();
         if (ACTION_UPDATE.equals(action)) {
-            getWeatherData(context);
+            getLocationData(context);
         }
     }
 
     @Override
     public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
-        getWeatherData(context);
+        getLocationData(context);
     }
 
-    void getWeatherData(final Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("FlutterSharedPreferences", Context.MODE_PRIVATE);
-        String locationDistrictBean = sharedPreferences.getString("flutter.location_district", "");
-        final District district = new Gson().fromJson(locationDistrictBean, District.class);
-        if (district == null) {
-            updateAppWidget(NO_LOCATION, context, "", null);
+    void getLocationData(Context context) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            updateAppWidget(NO_LOCATION, context, context.getString(R.string.widget_no_location_permission), null);
             return;
         }
 
-        // 显示正在刷新遮罩
-        updateAppWidget(UPDATING, context, district.name, null);
+        // 显示正在更新
+        updateAppWidget(UPDATING, context, "", null);
+
+        AMapLocationListener mLocationListener = aMapLocation -> {
+            if (aMapLocation.getErrorCode() == 0) {
+                String district = aMapLocation.getDistrict();
+                double longitude = aMapLocation.getLongitude();
+                double latitude = aMapLocation.getLatitude();
+                getWeatherData(context, district, longitude, latitude);
+            } else {
+                updateAppWidget(NO_LOCATION, context, context.getString(R.string.widget_location_failed), null);
+            }
+        };
+
+        AMapLocationClient locationClient = new AMapLocationClient(context);
+        locationClient.setLocationListener(mLocationListener);
+        AMapLocationClientOption locationOption = new AMapLocationClientOption();
+        locationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        locationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+        locationOption.setOnceLocation(true);
+        locationClient.setLocationOption(locationOption);
+        locationClient.stopLocation();
+        locationClient.startLocation();
+    }
+
+    void getWeatherData(final Context context, String district, double longitude, double latitude) {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.caiyunapp.com/v2/TwsDo9aQUYewFhV8/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService apiService = retrofit.create(ApiService.class);
-        Call<WeatherBean> call = apiService.weather(district.longitude, district.latitude);
+        Call<WeatherBean> call = apiService.weather(longitude, latitude);
         call.enqueue(new Callback<WeatherBean>() {
             @Override
             public void onResponse(@NonNull Call<WeatherBean> call, @NonNull Response<WeatherBean> response) {
                 WeatherBean weatherBean = response.body();
-                updateAppWidget(UPDATE_SUCCESS, context, district.name, weatherBean);
+                updateAppWidget(UPDATE_SUCCESS, context, district, weatherBean);
             }
 
             @Override
             public void onFailure(@NonNull Call<WeatherBean> call, @NonNull Throwable t) {
-                updateAppWidget(UPDATE_FAIL, context, district.name, null);
+                updateAppWidget(UPDATE_FAIL, context, district, null);
             }
         });
     }
@@ -109,20 +134,23 @@ public class AppWidget extends AppWidgetProvider {
         PendingIntent updatePendingIntent = PendingIntent.getBroadcast(context, 0, updateIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         if (status == NO_LOCATION) {
-            remoteViews.setTextViewText(R.id.widget_district_tv, "未知");
-            remoteViews.setTextViewText(R.id.widget_update_time_tv, "无定位");
+            remoteViews.setTextViewText(R.id.widget_district_tv, district);
+            remoteViews.setTextViewText(R.id.widget_update_time_tv, context.getString(R.string.widget_unknown));
+            remoteViews.setOnClickPendingIntent(R.id.widget_rl, launchPendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.widget_update_fl, updatePendingIntent);
 
             RemoteViews animViewDefault = new RemoteViews(context.getPackageName(), R.layout.app_widget_anim_view_default);
             remoteViews.removeAllViews(R.id.widget_update_anim_fl);
             remoteViews.addView(R.id.widget_update_anim_fl, animViewDefault);
         } else if (status == UPDATING) {
-            remoteViews.setTextViewText(R.id.widget_update_time_tv, "正在更新");
+            remoteViews.setTextViewText(R.id.widget_update_time_tv, context.getString(R.string.widget_updating));
+            remoteViews.setOnClickPendingIntent(R.id.widget_rl, launchPendingIntent);
 
             RemoteViews animView = new RemoteViews(context.getPackageName(), R.layout.app_widget_anim_view);
             remoteViews.removeAllViews(R.id.widget_update_anim_fl);
             remoteViews.addView(R.id.widget_update_anim_fl, animView);
         } else if (status == UPDATE_SUCCESS) {
-            showAppWidgetData(remoteViews, district, weatherBean);
+            showAppWidgetData(context, remoteViews, district, weatherBean);
             remoteViews.setOnClickPendingIntent(R.id.widget_rl, launchPendingIntent);
             remoteViews.setOnClickPendingIntent(R.id.widget_update_fl, updatePendingIntent);
 
@@ -130,7 +158,8 @@ public class AppWidget extends AppWidgetProvider {
             remoteViews.removeAllViews(R.id.widget_update_anim_fl);
             remoteViews.addView(R.id.widget_update_anim_fl, animViewDefault);
         } else if (status == UPDATE_FAIL) {
-            remoteViews.setTextViewText(R.id.widget_update_time_tv, "更新失败");
+            remoteViews.setTextViewText(R.id.widget_update_time_tv, context.getString(R.string.widget_update_failed));
+            remoteViews.setOnClickPendingIntent(R.id.widget_rl, launchPendingIntent);
             remoteViews.setOnClickPendingIntent(R.id.widget_update_fl, updatePendingIntent);
 
             RemoteViews animViewDefault = new RemoteViews(context.getPackageName(), R.layout.app_widget_anim_view_default);
@@ -140,7 +169,7 @@ public class AppWidget extends AppWidgetProvider {
         AppWidgetManager.getInstance(context).updateAppWidget(componentName, remoteViews);
     }
 
-    private void showAppWidgetData(RemoteViews remoteViews, String district, WeatherBean weatherBean) {
+    private void showAppWidgetData(Context context, RemoteViews remoteViews, String district, WeatherBean weatherBean) {
         Realtime realtime = weatherBean.result.realtime;
         Daily daily = weatherBean.result.daily;
 
@@ -149,7 +178,7 @@ public class AppWidget extends AppWidgetProvider {
         remoteViews.setTextViewText(R.id.widget_temperature_tv, toInt(realtime.temperature) + "°");
 
         String updateDate = DateUtils.getFormatDate(weatherBean.server_time * 1000, DateUtils.HHmm);
-        remoteViews.setTextViewText(R.id.widget_update_time_tv, updateDate + " 更新");
+        remoteViews.setTextViewText(R.id.widget_update_time_tv, context.getString(R.string.widget_update_time, updateDate));
 
         remoteViews.setTextViewText(R.id.widget_max_min_tv, toInt(daily.temperature.get(0).max)
                 + " / " + toInt(daily.temperature.get(0).min) + "°");
@@ -157,7 +186,7 @@ public class AppWidget extends AppWidgetProvider {
         // 明天预报
         Skycon skycon1 = daily.skycon.get(1);
         Temperature temperature1 = daily.temperature.get(1);
-        remoteViews.setTextViewText(R.id.widget_forecast_day_1_tv, "明天");
+        remoteViews.setTextViewText(R.id.widget_forecast_day_1_tv, context.getString(R.string.widget_tomorrow));
         remoteViews.setImageViewResource(R.id.widget_forecast_icon_1_iv, ImageUtils.getWeatherIcon(skycon1.value));
         remoteViews.setTextViewText(R.id.widget_forecast_temperature_1_iv, toInt(temperature1.max)
                 + " / " + toInt(temperature1.min) + "°");
@@ -199,3 +228,5 @@ public class AppWidget extends AppWidgetProvider {
     }
 }
 
+//        SharedPreferences sharedPreferences = context.getSharedPreferences("FlutterSharedPreferences", Context.MODE_PRIVATE);
+//        String locationDistrictBean = sharedPreferences.getString("flutter.location_district", "");
