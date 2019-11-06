2ac496e532723f9453b40f5fb2982451f65855ab
670453367 <670453367@qq.com>
2019-11-06 22:34:15 +0800

    删除无效图片资源

--- android/app/src/main/java/www/gl/com/coolweather/utils/ImageUtils.java
+++ android/app/src/main/java/www/gl/com/coolweather/utils/ImageUtils.java
@@ -8,29 +8,29 @@ public class ImageUtils {
         switch (weather) {
             case "CLEAR_DAY":
             case "CLEAR_NIGHT":
-                iconId = R.drawable.sunny_ic;
+                iconId = R.drawable.ic_sunny;
                 break;
             case "PARTLY_CLOUDY_DAY":
             case "PARTLY_CLOUDY_NIGHT":
-                iconId = R.drawable.cloud_ic;
+                iconId = R.drawable.ic_cloud;
                 break;
             case "CLOUDY":
-                iconId = R.drawable.nosun_ic;
+                iconId = R.drawable.ic_nosun;
                 break;
             case "WIND":
-                iconId = R.drawable.wind_ic;
+                iconId = R.drawable.ic_wind;
                 break;
             case "HAZE":
-                iconId = R.drawable.haze_ic;
+                iconId = R.drawable.ic_haze;
                 break;
             case "RAIN":
-                iconId = R.drawable.rain_ic;
+                iconId = R.drawable.ic_rain;
                 break;
             case "SNOW":
-                iconId = R.drawable.snow_ic;
+                iconId = R.drawable.ic_snow;
                 break;
             default:
-                iconId = R.drawable.sunny_ic;
+                iconId = R.drawable.ic_sunny;
                 break;
         }
         return iconId;
@@ -40,54 +40,54 @@ public class ImageUtils {
         int bgResId;
         switch (weather) {
             case "CLEAR_DAY":
-                bgResId = R.drawable.bkg_sunny_widget;
+                bgResId = R.drawable.bg_widget_sunny;
                 break;
             case "CLEAR_NIGHT":
-                bgResId = R.drawable.bkg_sunny_night_widget;
+                bgResId = R.drawable.bg_widget_sunny_night;
                 break;
             case "PARTLY_CLOUDY_DAY":
-                bgResId = R.drawable.bkg_cloudy_widget;
+                bgResId = R.drawable.bg_widget_cloudy;
                 break;
             case "PARTLY_CLOUDY_NIGHT":
-                bgResId = R.drawable.bkg_cloudy_night_widget;
+                bgResId = R.drawable.bg_widget_cloudy_night;
                 break;
             case "CLOUDY":
-                bgResId = isDay ? R.drawable.bkg_overcast_widget : R.drawable.bkg_overcast_night_widget;
+                bgResId = isDay ? R.drawable.bg_widget_overcast : R.drawable.bg_widget_overcast_night;
                 break;
             case "WIND":
-                bgResId = isDay ? R.drawable.bkg_sunny_widget : R.drawable.bkg_sunny_night_widget;
+                bgResId = isDay ? R.drawable.bg_widget_sunny : R.drawable.bg_widget_sunny_night;
                 break;
             case "HAZE":
-                bgResId = isDay ? R.drawable.bkg_haze_widget : R.drawable.bkg_haze_night_widget;
+                bgResId = isDay ? R.drawable.bg_widget_haze : R.drawable.bg_widget_haze_night;
                 break;
             case "RAIN":
                 if (0.03 < intensity && intensity < 0.25) {
-                    bgResId = isDay ? R.drawable.bkg_drizzle_widget : R.drawable.bkg_drizzle_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_drizzle : R.drawable.bg_widget_drizzle_night;
                 } else if (0.25 < intensity && intensity < 0.35) {
-                    bgResId = isDay ? R.drawable.bkg_rain_widget : R.drawable.bkg_rain_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_rain : R.drawable.bg_widget_rain_night;
                 } else if (0.35 < intensity && intensity < 0.48) {
-                    bgResId = isDay ? R.drawable.bkg_downpour_widget : R.drawable.bkg_downpour_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_downpour : R.drawable.bg_widget_downpour_night;
                 } else if (0.48 < intensity) {
-                    bgResId = isDay ? R.drawable.bkg_rainstorm_widget : R.drawable.bkg_rainstorm_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_rainstorm : R.drawable.bg_widget_rainstorm_night;
                 } else {
-                    bgResId = isDay ? R.drawable.bkg_rain_widget : R.drawable.bkg_rain_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_rain : R.drawable.bg_widget_rain_night;
                 }
                 break;
             case "SNOW":
                 if (0.03 < intensity && intensity < 0.25) {
-                    bgResId = isDay ? R.drawable.bkg_snow_widget : R.drawable.bkg_snow_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_snow : R.drawable.bg_widget_snow_night;
                 } else if (0.25 < intensity && intensity < 0.35) {
-                    bgResId = isDay ? R.drawable.bkg_snow_widget : R.drawable.bkg_snow_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_snow : R.drawable.bg_widget_snow_night;
                 } else if (0.35 < intensity && intensity < 0.48) {
-                    bgResId = isDay ? R.drawable.bkg_snow_widget : R.drawable.bkg_snow_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_snow : R.drawable.bg_widget_snow_night;
                 } else if (0.48 < intensity) {
-                    bgResId = isDay ? R.drawable.bkg_snow_widget : R.drawable.bkg_snow_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_snow : R.drawable.bg_widget_snow_night;
                 } else {
-                    bgResId = isDay ? R.drawable.bkg_snow_widget : R.drawable.bkg_snow_night_widget;
+                    bgResId = isDay ? R.drawable.bg_widget_snow : R.drawable.bg_widget_snow_night;
                 }
                 break;
             default:
-                bgResId = R.drawable.bkg_sunny_widget;
+                bgResId = R.drawable.bg_widget_sunny;
                 break;
         }
         return bgResId;
