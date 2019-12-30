0b295d475f4da83c9474803d04587217abb92efd
GoodLuck-GL <670453367@qq.com>
2019-12-30 15:03:15 +0800

    APP图标动画改为非线性

--- lib/test.dart
+++ lib/test.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nima/nima_actor.dart';
 
 class TestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("超人"),
+        title: new Text("Title"),
       ),
-      body: NimaActor(
-        "assets/Robot.nima",
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: "Flight",
-      ),
+      body: Container(),
     );
   }
 }
