8953b10846a67deb776baebb9959f679a4663612
GaozyDev <670453367@qq.com>
2019-12-31 14:55:17 +0800

    继承极光推送

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:coolweather/main_page.dart';
-import 'package:coolweather/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'about_page.dart';
@@ -12,7 +12,6 @@ import 'setting_page.dart';
 import 'data/unit_model.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   /// 单位model
@@ -21,6 +20,8 @@ void main() {
 
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
+
+  initJPush();
 
   runApp(ChangeNotifierProvider<UnitModel>.value(
     value: unitModel,
@@ -40,7 +41,7 @@ void main() {
       routes: {
 //        "/": (context) => TestPage(),
         "/": (context) => MainPage(),
-        "more_day_forecast":(context) => MoreDayForecast(),
+        "more_day_forecast": (context) => MoreDayForecast(),
         "focus_district_list": (context) => FocusDistrictListPage(),
         "select_district": (context) => AddDistrictPage(),
         "setting": (context) => SettingPage(),
@@ -48,4 +49,29 @@ void main() {
       },
     ),
   ));
+}
+
+initJPush() {
+  JPush jpush = new JPush();
+  jpush.addEventHandler(
+    // 接收通知回调方法。
+    onReceiveNotification: (Map<String, dynamic> message) async {
+      print("flutter onReceiveNotification: $message");
+    },
+    // 点击通知回调方法。
+    onOpenNotification: (Map<String, dynamic> message) async {
+      print("flutter onOpenNotification: $message");
+    },
+    // 接收自定义消息回调方法。
+    onReceiveMessage: (Map<String, dynamic> message) async {
+      print("flutter onReceiveMessage: $message");
+    },
+  );
+  jpush.setup(
+    appKey: "43ae1d5376e4359e8bd732c1",
+    channel: "theChannel",
+    production: false,
+    debug: true,
+  );
+  jpush.getRegistrationID().then((rid) { });
 }
