f9ab254a55403693cda8d692f025abf8761babdc
GaozyDev <670453367@qq.com>
2020-01-15 09:17:55 +0800

    升级版本号到1.5.4+17

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -85,7 +85,8 @@ class AboutPageState extends State<AboutPage> {
                           '2.优化6天天气预报布局\n'
                           '3.优化日出日落图标显示大小问题\n'
                           '4.添加更多动画资源，丰富用户体验\n'
-                          '5.修复了其他一些已知的小bug\n',
+                          '5.修复了其他一些已知的小bug\n'
+                          '6.移除极光推送Flutter插件，该插件目前问题较多，会造成部分机型闪退\n',
                           softWrap: true,
                           style: TextStyle(fontSize: 13),
                         ),
